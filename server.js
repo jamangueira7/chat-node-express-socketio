0c6aa4f4dcfee244b21f356c0b2b9d908e3c3b60
João Mangueira <jamjs7@gmail.com>
2021-02-19 12:10:38 -0300

    mensagem para sair do chat

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser } = require('./utils/users');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -41,7 +46,14 @@ io.on('connection', socket => {
 
     //rodar quando um client desconectar
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName,'Um usuario saiu do chat!'));
+        const user = userLeave(socket.id);
+
+        if(user) {
+            io.to(user.room).emit(
+                'message',
+                formatMessage(botName,`O usuario ${user.username} saiu do chat!`)
+            );
+        }
     });
 });
 
